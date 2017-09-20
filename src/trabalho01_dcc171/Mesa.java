ba36fc9b65c86d531cd0ab1d21bbabbab5c13b3c
pedrofreitas <pedrofreitas@10.148.220.95>
2017-09-20 11:00:22 -0300

    Começando o trabalho

---
+++ src/trabalho01_dcc171/Mesa.java
@@ -0,0 +1,12 @@
+
+package trabalho01_dcc171;
+
+/*
+Essa classe representará as mesas do estabelecimento sem levar em consideração a
+quantidade de pessoas na mesa, ou quantas pessoas cabem na mesa.
+ */
+public class Mesa {
+    private int numMesa; //essa variável vai controlar o fluxo de mesas.
+    private boolean pedido; // enquanto for true, podem ser adicionados mais itens ao pedido
+    
+}
