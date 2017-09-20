61c5808c283a3eb1fbf931c0c2e8bd9b527ea6b5
pedrofreitas <pedrofreitas@10.148.220.95>
2017-09-20 10:17:17 -0300

    Começando o trabalho

---
+++ src/trabalho01_dcc171/Trabalho01_DCC171.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trabalho01_dcc171;
+
+/**
+ *
+ * @author pedrofreitas
+ */
+public class Trabalho01_DCC171 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
